e98fde5e721ea57cc36a4cde387ea6b40fba2753
Tanmay Daga <tanmay06daga@gmail.com>
2024-11-27 17:44:58 +0530

    initial commit

---
+++ src/main/java/in/ac/iitj/instiapp/InstiappApplication.java
@@ -0,0 +1,13 @@
+package in.ac.iitj.instiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstiappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ac/iitj/instiapp/InstiappApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ac.iitj.instiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
