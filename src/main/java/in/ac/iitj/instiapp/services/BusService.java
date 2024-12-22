55877d7f6eba62d81e633d493b5b115f84c56766
nayan-kute21 <nayankishorkute21@gmail.com>
2024-12-22 13:02:11 +0530

    service layer for bus along with test

--- src/main/java/in/ac/iitj/instiapp/services/BusService.java
+++ src/main/java/in/ac/iitj/instiapp/services/BusService.java
@@ -1,29 +1,138 @@
 package in.ac.iitj.instiapp.services;
 
-import in.ac.iitj.instiapp.database.entities.Scheduling.Buses.BusLocation;
-import in.ac.iitj.instiapp.database.entities.Scheduling.Buses.BusRun;
-import in.ac.iitj.instiapp.database.entities.Scheduling.Buses.BusSchedule;
-import in.ac.iitj.instiapp.database.entities.Scheduling.Buses.ScheduleType;
+import in.ac.iitj.instiapp.database.entities.Scheduling.Buses.*;
+import in.ac.iitj.instiapp.payload.Scheduling.Buses.BusOverrideDto;
+import in.ac.iitj.instiapp.payload.Scheduling.Buses.BusScheduleDto;
 import org.springframework.data.domain.Pageable;
 
 import java.sql.Time;
 import java.util.List;
 
 public interface BusService {
-    void createBusLocation(String name);
-    void removeBusLocation(String name);
-    BusLocation findBusLocation(String name);
-    List<String> listBusLocations(Pageable pageable);
-    boolean checkBusLocationExists(String name);
 
-    void createBusSchedule(BusSchedule busSchedule);
-    BusSchedule findBusSchedule(String busNumber);
-    List<BusSchedule> listBusSchedules(Pageable pageable);
+/*----------------------------------------------BUS LOCATION----------------------------------------------------------*/
+    /**
+     * @param name of Location to e inserted
+     */
+    void saveBusLocation(String name);
 
-    void createBusRun(BusRun busRun);
-    void updateBusScheduleRun(String busNumber, ScheduleType scheduleType, Time timeOfDeparture, BusRun busRun);
+    /**
+     * @param pageable  firstResult Max Value-INT_MAX,Min Value-0 PageSize-Max value-TODO
+     * @return Returns a list od all the bus locations (Pagination implementation)
+     */
+    List<String> getBusLocations(Pageable pageable);
 
-    boolean checkBusScheduleExists(String busNumber);
-    void removeBusSchedule(String busNumber);
-    void updateBusScheduleNumber(String oldBusNumber, String newBusNumber);
+    /**
+     * @param name
+     * @return Returns the id of the bus locations else returns -1
+     */
+    Long isBusLocationExist(String name);
+
+    /**
+     * @param oldName
+     * @param newName
+     * need to implement the cascading effect
+     * AutoUpdates the bus run and bus override as the id remains same
+     */
+    void updateBusLocation(String oldName, String newName);
+
+    /**
+     * @param name
+     * Delete the bus run and bus override run of that locations IMPLEMENTED IN REPOSITORY
+     */
+    void deleteBusLocation(String name);
+
+/*--------------------------------------------------BUS SCHEDULE------------------------------------------------------*/
+    /**
+     * @param busNumber
+     */
+    void saveBusSchedule(String busNumber);
+
+    /**
+     * @param busNumber
+     * @return BusScheduleDto form the bus run table
+     */
+    BusScheduleDto getBusSchedule(String busNumber);
+
+    /**
+     * @param pageable
+     * @return List of Name of current busses
+     */
+    List<String> getBusNumbers(Pageable pageable);
+
+    /**
+     * @param busNumber
+     * @return Returns id if the bus number exists else return -1
+     */
+    Long existsBusSchedule(String busNumber);
+
+    /**
+     * @param oldBusNumber
+     * @param newBusNumber
+     * Autoupdates the busSchedule in bus override and bus run
+     */
+    void updateBusSchedule(String oldBusNumber,String newBusNumber);
+
+    /**
+     * @param busNumber
+     * Deletes BusRun and BusOverrride of that bus number IMPLEMENTED IN REPOSITORY
+     */
+    void deleteBusSchedule(String busNumber);
+/*---------------------------------------------------BUS RUN----------------------------------------------------------*/
+
+    /**
+     * @param busRun
+     * @param busNumber
+     * @Assumtions Assuming the Bus Run object contains the correct name of location and bus run to get refrence from
+     */
+    void saveBusRun(BusRun busRun,String busNumber);
+
+    /**
+     * @param publicId
+     * @return True if bus run with that public id exist else False
+     */
+    Boolean existsBusRunByPublicId(String publicId);
+
+    /**
+     * @param publicId
+     * @param newBusRun
+     *
+     */
+    void updateBusScheduleRun(String publicId, BusRun newBusRun);
+
+    /**
+     * @param busRunPublicIds List af all the bus runs public id to be deleted
+     */
+    void deleteBusRuns(List<String> busRunPublicIds);
+
+/*--------------------------------------------BUS OVERRIDE------------------------------------------------------------*/
+    /**
+     * @param busNumber
+     * @param busOverride
+     */
+    void saveBusOverride(String busNumber, BusOverride busOverride);
+
+    /**
+     * @param publicId
+     * @return
+     */
+    boolean existsBusOverrideByPublicId(String publicId);
+
+    /**
+     * @param year
+     * @param month
+     * @return
+     */
+    List<BusOverrideDto> getBusOverrideForYearAndMonth(int year, int month);
+
+    /**
+     * @param publicId
+     * @param newBusOverride
+     */
+    void updateBusOverride(String publicId,BusOverride newBusOverride);
+
+    /**
+     * @param busOverrideIds
+     */
+    void deleteBusOverride(List<String> busOverrideIds);
 }
