1d692897d751f14867a85db4c9e9774cb00b4803
DarkPhoenix616 <daniegeorgejohn@gmail.com>
2025-01-06 19:48:32 +0530

    Adding GrievanceTests

---
+++ src/test/java/in/ac/iitj/instiapp/Tests/EntityTestData/OrganisationRoleData.java
@@ -0,0 +1,28 @@
+package in.ac.iitj.instiapp.Tests.EntityTestData;
+
+import in.ac.iitj.instiapp.database.entities.User.Organisation.OrganisationPermission;
+import in.ac.iitj.instiapp.database.entities.User.Organisation.OrganisationRole;
+import static in.ac.iitj.instiapp.database.entities.User.Organisation.OrganisationPermission.*;
+
+
+
+public enum OrganisationRoleData {
+    ORGANISATION_ROLE1("Coordinator",MASTER),
+    ORGANISATION_ROLE2("Vice President",INTERMEDIATE),
+    ORGANISATION_ROLE3("President",READ),
+    ORGANISATION_ROLE4("Coordinator",INTERMEDIATE);
+
+
+    public final String roleName;
+    public final OrganisationPermission organisationPermission;
+
+    OrganisationRoleData(String roleName, OrganisationPermission organisationPermission){
+        this.roleName = roleName;
+        this.organisationPermission = organisationPermission;
+    }
+
+    public OrganisationRole toEntity() {
+        return new OrganisationRole(null, null, this.roleName, this.organisationPermission);
+    }
+
+}
